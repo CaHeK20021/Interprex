a2b1a1e519c0fcaaff91a35ceadf5b591e7344d5
CaHeK20021 <kolpakov.23003286@inbox.ru>
2026-06-19 19:44:28 +0500

    update

--- set_pass.ps1
+++
@@ -1,3 +0,0 @@
-[System.Environment]::SetEnvironmentVariable('TAURI_SIGNING_PRIVATE_KEY_PASSWORD', '', 'User')
-$p = [System.Environment]::GetEnvironmentVariable('TAURI_SIGNING_PRIVATE_KEY_PASSWORD', 'User')
-if ($null -ne $p) { Write-Host "PASS_SET: '$p' ($($p.Length) chars)" } else { Write-Host "STILL_MISSING" }
